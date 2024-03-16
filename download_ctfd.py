32837c2bed00193fe265b27fe5db7501e7fd78a1
Chris Greene <archgoon@gmail.com>
2024-03-16 10:54:21 -0700

    Switch to httpx. hyper is a dead project.
    Fix default skip argument.

--- download_ctfd.py
+++ download_ctfd.py
@@ -4,16 +4,11 @@ import json
 import os
 import sys
 import pathlib
-import collections.abc
-#hyper needs the four following aliases to be done manually.
-collections.Iterable = collections.abc.Iterable
-collections.Mapping = collections.abc.Mapping
-collections.MutableSet = collections.abc.MutableSet
-collections.MutableMapping = collections.abc.MutableMapping
-from hyper.contrib import HTTP20Adapter
+import httpx
 
-def download(s, source, dest):
-    res = s.get(source)
+
+def download(cookies, source, dest):
+    res = httpx.get(source, cookies=cookies, follow_redirects=True)
     with open(dest, "wb") as target_file:
         target_file.write(res.content)
 
@@ -24,6 +19,7 @@ def main():
     parser.add_argument("--root-url", required=True, help="url of challenges")
     parser.add_argument("--api-url", action="store_true", default=None)
     parser.add_argument("--root-dir", required=True, help="target output dir")
+    parser.add_argument("--skip", action='append', default=[])
     options = parser.parse_args()
     session = options.session
     if options.api_url is None:
@@ -31,17 +27,17 @@ def main():
 
     if not os.path.exists(options.root_dir):
         pathlib.Path(options.root_dir).mkdir(parents=True, exist_ok=True)       
-    
-    s = requests.Session()
-    s.mount(f'{options.api_url}', HTTP20Adapter())
 
+    cookies = {"session":session}
+    
     print(f"Obtaining challenges from {options.api_url}/challenges")
-    challenges = s.get(f"{options.api_url}/challenges", headers = {
-                "Cookie": f"session={session}",
-            })
+    challenges = httpx.get(f"{options.api_url}/challenges", cookies=cookies, follow_redirects=True)
     problems = json.loads(challenges.content)
 
     for chal in problems["data"]:
+        if chal["name"] in options.skip:
+            print(f"Skipping {chal['name']}")
+            continue
         print(f"Downloading {chal['name']}...")
         target_dir = os.path.join(options.root_dir, chal["name"])
         if not os.path.exists(target_dir):
@@ -51,10 +47,8 @@ def main():
                 raise Exception("mkdir failed")
         problem_filename = f"{target_dir}/problem.json"
         if not os.path.exists(problem_filename) or options.force:
-            result = s.get(f"{options.api_url}/challenges/{chal['id']}",
-                headers = {
-                    "Cookie": f"session={session};"
-                }).content
+            result = httpx.get(f"{options.api_url}/challenges/{chal['id']}",
+                cookies=cookies, follow_redirects=True).content
             with open(problem_filename, "wb") as probfile:
                 probfile.write(result)
         else:
@@ -67,7 +61,7 @@ def main():
                 print(f"'{file_name}' is downloaded")
                 continue
             print(f"'{file_name}' is being downloaded...")
-            download(s, f"{options.root_url}/{f}", f"{target_dir}/{file_name}")
+            download({"session":session}, f"{options.root_url}/{f}", f"{target_dir}/{file_name}")
 
 if __name__ == "__main__":
     main()
